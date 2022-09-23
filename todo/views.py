9f50e53608049a1b92307e6a7099b63de57bf323
José Loarca <joseloarca97@icloud.com>
2022-09-22 18:51:39 -0600

    added endpoint to update todos parents status

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Todo
 from .serializers import TodoSerializer
@@ -9,3 +11,11 @@ class TodoViewSet(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     permission_classes = []
     filterset_fields = ['id', 'name', 'is_complete', 'parent', 'children']
+
+    @action(detail=True, methods=['put'])
+    def parents(self, request, pk=None):
+        """ Update all its parent status(is_complete field) to be the same as the received TODO. """
+        todo = self.get_object()
+        update_parents = todo.update_parents()
+
+        return Response(update_parents['response'], status=update_parents['status_code'])
